41808e18f8340b111e13aa3bc6db0aea4b34e54e
Anandamaya <arnoananda@gmail.com>
2020-05-22 19:48:44 +0200

    inClass exercises finished

--- InClass/C-fetch/exercise.js
+++ InClass/C-fetch/exercise.js
@@ -10,3 +10,26 @@ When you get the response from the server, print the current temperature in an <
 
 ================
 */
+
+var btnEl = document.querySelector("button");
+btnEl.addEventListener("click", getTemperature  );
+
+function getTemperature(event){
+    var latitude = document.querySelector("#latitude").value;
+    var longitude = document.querySelector("#longitude").value;
+
+    var url = "https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude;
+
+    var result;
+    
+    fetch(url)
+        .then(response => response.json())
+        .then(function(data){
+            var temperature = data.main.temp;
+            document.querySelector("h3").innerHTML = "The current temperature is "+temperature;
+        });
+
+    event.preventDefault();
+}
+
+
